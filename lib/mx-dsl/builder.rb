e4d1a0b4526c2908c272673d8987b1b117a993f0
giorgi <giorgi@lekishvili.info>
2008-12-11 22:33:35 +0400

    builder

--- lib/mx-dsl/builder.rb
+++ lib/mx-dsl/builder.rb
@@ -1,3 +1,24 @@
+# molecule "id" do
+#   atoms {
+#      comment Symb id-num R/S x y z
+#      C 1 0 0.234 0.345 0.456
+#      C 2 -1 0.235 1.345 1.23            
+#   }
+#   bonds {
+#      comment bond-type atoms
+#      single [1,2]
+#      poly [3,4,6,12]
+#   }
+#   properties {
+#      ID50 0.0023
+#      bp 2.34
+#   }
+#   descriptors {
+#      elstate :weight=>'gast-mars'
+#      autocorr :range=>[1,5.6], :weight=>'total-charge'
+#      wiener :3d=>true    
+#   }
+# end
 module MXdsl
 	module Builder
 		class Builder
